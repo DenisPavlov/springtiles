703e02e200b35d5cb8938f905073fb4d36cd9dde
DenisPavlov <denispavlov1989@gmail.com>
2019-03-27 17:19:52 +1000

    базовые сущности

--- src/main/java/com/habuma/spitter/mvc/HomeController.java
+++ src/main/java/com/habuma/spitter/mvc/HomeController.java
@@ -1,13 +1,41 @@
 package com.habuma.spitter.mvc;
 
+import java.util.Map;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.habuma.spitter.service.SpitterService;
+
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
+
+    private SpitterService spitterService;
+
+    @Inject
+    public HomeController(SpitterService spitterService) {
+        this.spitterService = spitterService;
+    }
+
+    @RequestMapping(value={"/","/home"}, method=RequestMethod.GET)
+    public String showHomePage(Map<String, Object> model) {
+        model.put("spittles", spitterService.getRecentSpittles(spittlesPerPage));
         return "home";
+    }
+
+
+    public static final int DEFAULT_SPITTLES_PER_PAGE = 25;
+
+    private int spittlesPerPage = DEFAULT_SPITTLES_PER_PAGE;
+
+    public void setSpittlesPerPage(int spittlesPerPage) {
+        this.spittlesPerPage = spittlesPerPage;
+    }
+
+    public int getSpittlesPerPage() {
+        return spittlesPerPage;
     }
 }
